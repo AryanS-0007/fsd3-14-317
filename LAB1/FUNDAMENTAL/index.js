c776996cabe1d0b1a0c5aaea96e3d74ed6ff2097
Aryan Singhaniya <singhaniya.aryan07@gmail.com>
2026-09-15 10:23:48 +0530

    Add LAB1 project files

---
+++ LAB1/FUNDAMENTAL/index.js
@@ -0,0 +1,20 @@
+//WRITE A FUNCTION TO TAKE YOUR NAME AND PRINT IT WITH HELLO
+function sayHello(name) {
+    console.log(`Hello ${name}`);
+}
+const sayHi = (name) => {
+    console.log(`Hi ${name}`);
+};
+
+//take two arguments and return their sum
+
+const findSum = (a, b) => {
+    return a + b;
+}
+const sum=(a,b)=>a+b;
+sayHello("ARYAN");
+sayHi("Aditya ");
+console.log("sum =",findSum(5,6));
+console.log("sum =",findSum(15,13));
+console.log("sum =",sum(4,5));
+console.log("sum =",sum(10,12));
